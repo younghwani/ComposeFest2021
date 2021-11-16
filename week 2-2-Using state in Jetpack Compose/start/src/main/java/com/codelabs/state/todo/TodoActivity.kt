237b75bc2775a36af82bbddb2feda44f7bbf32e2
Younghwan <younghwankim624@gmail.com>
2021-11-16 23:03:44 +0900

    StateCodelab
    9 end

--- week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoActivity.kt
+++ week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoActivity.kt
@@ -43,11 +43,10 @@ class TodoActivity : AppCompatActivity() {
 
     @Composable
     private fun TodoActivityScreen(todoViewModel: TodoViewModel) {
-        val items : List<TodoItem> by todoViewModel.todoItems.observeAsState(listOf())
         TodoScreen(
-            items = items,
-            onAddItem = { todoViewModel.addItem(it) },
-            onRemoveItem = { todoViewModel.removeItem(it) }
+            items = todoViewModel.todoItems,
+            onAddItem = todoViewModel::addItem,
+            onRemoveItem = todoViewModel::removeItem
         )
     }
 }
